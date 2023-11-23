20eb992ee596eac6a6b65b48685b7cfcdb549ba6
shafiulshaon <shafiulshaon@gmail.com>
2023-11-24 04:37:20 +0900

    Added Home page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <MainTemplate>
+    <ImageUploader />
+  </MainTemplate>
+</template>
+
+<script>
+import MainTemplate from '../templates/MainTemplate.vue';
+import ImageUploader from '../components/organisms/ImageUploader.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    MainTemplate,
+    ImageUploader
+  }
+}
+</script>
---
+++ src/templates/MainTemplate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-template">
+    <header>
+      <!-- header content -->
+    </header>
+    <main>
+      <!-- Slot for main content -->
+      <slot></slot>
+    </main>
+    <footer>
+      <!-- footer content -->
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainTemplate'
+}
+</script>
+
+<style scoped>
+.main-template header {
+  /* header styles */
+}
+.main-template main {
+  /* main content styles */
+  padding: 20px;
+  text-align: left;
+}
+.main-template footer {
+  /* footer styles */
+}
+</style>
