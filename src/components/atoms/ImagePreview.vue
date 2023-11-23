b4b7b9655019cce88a49bf1ce9ee803a25dcb7c3
shafiulshaon <shafiulshaon@gmail.com>
2023-11-24 04:41:06 +0900

    Added Image preview component

---
+++ src/components/atoms/ImagePreview.vue
@@ -0,0 +1,34 @@
+<template>
+  <div className="image-preview">
+    <img :src="image.url" :alt="altText"/>
+  </div>
+</template>
+
+<script>
+import {defineComponent, computed} from 'vue';
+
+export default defineComponent({
+  props: {
+    image: {
+      type: Object,
+      required: true
+    },
+    altText: {
+      type: String,
+      default: 'Uploaded image'
+    }
+  },
+  setup(props) {
+    const altTexts = computed(() => props.altText);
+
+    return { altTexts };
+  }
+});
+</script>
+
+<style scoped>
+.image-preview img {
+  max-width: 100px;
+  margin: 5px;
+}
+</style>
