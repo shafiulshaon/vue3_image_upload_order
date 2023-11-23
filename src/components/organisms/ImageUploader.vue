4caba2d0b05279dbf7087e32c88139572bbcd6a0
shafiulshaon <shafiulshaon@gmail.com>
2023-11-24 04:44:24 +0900

    added Drag-and-Drop Reordering with HTML5 API

--- src/components/organisms/ImageUploader.vue
+++ src/components/organisms/ImageUploader.vue
@@ -2,23 +2,24 @@
   <div>
     <input type="file" multiple @change="handleFileUpload" />
     <div v-if="images.length">
-      <image-upload-control
-        v-for="(image, index) in images"
-        :key="image.url"
-        :image="image"
-        :index="index"
-        @remove="removeImage"
-      />
+      <ImageUploadControl
+      v-for="(image, index) in images"
+      :key="index"
+      :image="image"
+      :index="index"
+      @remove="removeImage"
+      @reorder="reorderImages"
+    />
     </div>
     <button @click="uploadImages">Upload</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import {ref, defineComponent, onBeforeUnmount} from 'vue';
 import ImageUploadControl from '../molecules/ImageUploadControl.vue';
 
-export default {
+export default defineComponent({
   components: {
     ImageUploadControl
   },
@@ -26,12 +27,19 @@ export default {
     const images = ref([]);
 
     const handleFileUpload = (event) => {
-      const files = Array.from(event.target.files);
+      const files = Array.from(event.target.files).filter(file => file.type.startsWith('image/'));
+
+      if (files.length === 0) {
+        // No images selected, show an error message
+        return;
+      }
+
       images.value.push(...files.map(file => ({
         url: URL.createObjectURL(file),
         file: file
       })));
     };
+
 
     const removeImage = (index) => {
       images.value.splice(index, 1);
@@ -41,14 +49,26 @@ export default {
       // Logic to upload images
     };
 
+
+    const reorderImages = ({ originIndex, targetIndex }) => {
+      const origin = images.value[originIndex];
+      images.value.splice(originIndex, 1);
+      images.value.splice(targetIndex, 0, origin);
+    };
+
+    onBeforeUnmount(() => {
+      images.value.forEach(image => URL.revokeObjectURL(image.url));
+    });
+
     return {
       images,
       handleFileUpload,
       removeImage,
+      reorderImages,
       uploadImages
     };
   }
-};
+});
 </script>
 
-<!-- Add styles for the uploader if needed -->
+<!-- styles for the uploader -->
