603bbcd37bb61a5a2bdae4dbf1e607a1ab923551
shafiulshaon <shafiulshaon@gmail.com>
2023-11-27 01:52:56 +0900

    Added button component

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button :class="btnType" @click="onClick">
+    <slot></slot>
+    {{ title }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    title: String,
+    btnType: {
+      type: String,
+      default: 'btn'
+    }
+  },
+  emits: ['click'],
+  methods: {
+    onClick(event) {
+      this.$emit('click', event);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
