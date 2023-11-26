61dbbfeda0e1b4812d2f35c34d1be5932a24e947
shafiulshaon <shafiulshaon@gmail.com>
2023-11-27 01:53:43 +0900

    Added separate icon file to organize the icons in one place

---
+++ src/assets/icons.js
@@ -0,0 +1,4 @@
+export const deleteIcon = require('@/assets/images/delete.svg')
+export const leftArrowIcon = require('@/assets/images/left-arrow.png')
+export const rightArrowIcon = require('@/assets/images/right-arrow.png')
+export const uploadFileIcon = require('@/assets/images/upload-file.svg')
